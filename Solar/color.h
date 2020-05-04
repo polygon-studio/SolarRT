5ac2e37aafe1437b1044af085a8328ad6fa5c56e
polygon-studio <developer@polygon.studio>
2020-05-04 14:20:37 -0700

    Added color from raycast

--- Solar/color.h
+++ Solar/color.h
@@ -5,6 +5,6 @@
 void write_color(std::ostream& out, color pixel_color) {
 	// write the translated [0,255] value of each color component.
 	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-		<< static_cast<int>(255.999 * pixel_color.x()) << ' '
-		<< static_cast<int>(255.999 * pixel_color.x()) << '\n';
+		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
+		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
 }
--- Solar/main.cpp
+++ Solar/main.cpp
@@ -2,20 +2,34 @@
 #include <fstream>
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main() {
 	std::ofstream file;
 	file.open("render.ppm");
 
-	const int image_width = 256;
-	const int image_height = 256;
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 384;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
 	file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+	point3 origin(0.0, 0.0, 0.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.25, 0.0);
+	point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, 1);
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			color pixel_color = ray_color(r);
 			write_color(file, pixel_color);
 		}
 	}
