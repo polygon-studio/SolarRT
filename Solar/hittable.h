77011756d1e6fffef7cde8474ad074cab147a172
polygon-studio <developer@polygon.studio>
2020-05-04 22:02:39 -0700

    Added face normal detection

--- Solar/hittable.h
+++ Solar/hittable.h
@@ -4,7 +4,13 @@
 struct hit_record {
 	point3 p;
 	vec3 normal;
+	bool front_face;
 	double t;
+
+	inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+		front_face = dot(r.direction(), outward_normal) < 0;
+		normal = front_face ? outward_normal : -outward_normal;
+	}
 };
 
 class hittable {
