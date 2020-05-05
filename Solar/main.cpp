9fecd804c27b7bda1c4ca23f51718648c7916dfe
polygon-studio <developer@polygon.studio>
2020-05-05 00:11:44 -0700

    Gamma correction

--- Solar/main.cpp
+++ Solar/main.cpp
@@ -12,7 +12,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 	// If we've exceeded the ray bound limit, no more light is gathered
 	if (depth <= 0) { return color(0, 0, 0); }
 
-	if (world.hit(r, 0, infinity, rec)) {
+	if (world.hit(r, 0.001, infinity, rec)) {
 		point3 target = rec.p + rec.normal + random_in_unit_sphere();
 		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
 	}
@@ -57,5 +57,4 @@ int main() {
 	}
 	std::cerr << "\nDone.\n";
 	file.close();
-
 }
