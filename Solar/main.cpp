34e290cd56293bc4839573f46f342efd9de698b5
polygon-studio <developer@polygon.studio>
2020-05-04 23:49:32 -0700

    Added diffuse shading

--- Solar/main.cpp
+++ Solar/main.cpp
@@ -7,10 +7,14 @@
 #include "hittable_list.h"
 #include "sphere.h"
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
 	hit_record rec;
+	// If we've exceeded the ray bound limit, no more light is gathered
+	if (depth <= 0) { return color(0, 0, 0); }
+
 	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + color(1, 1, 1));
+		point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -25,6 +29,7 @@ int main() {
 	const int image_width = 384;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 100;
+	const int max_depth = 50;
 
 	file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -45,7 +50,7 @@ int main() {
 				auto u = (i + random_double()) / (image_width - 1);
 				auto v = (j + random_double()) / (image_height - 1);
 				ray r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, world);
+				pixel_color += ray_color(r, world, max_depth);
 			}
 			write_color(file, pixel_color, samples_per_pixel);
 		}
